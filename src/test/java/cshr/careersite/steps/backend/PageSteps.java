cb8f17219cb48f31cca351a2a4108acd71039b2a
Usha Manjunath <usha.manjunath@gmail.com>
2018-07-18 14:11:42 +0100

    Adjust step for mobile and desktop

--- src/test/java/cshr/careersite/steps/backend/PageSteps.java
+++ src/test/java/cshr/careersite/steps/backend/PageSteps.java
@@ -173,99 +173,6 @@ public class PageSteps {
 
     }
 
-  /*  @Step
-    public void fillFormFields(List<PageTemplateObject> pageTemplateObject)
-    {
-        String oldSectionName = "";
-
-        for (PageTemplateObject aPageTemplateObject : pageTemplateObject) {
-            String[] sectionNames = aPageTemplateObject.sections_sub_sections.split(",");
-            departmentTemplatePage.selectTab(sectionNames[0]);
-
-            // Get CSS string for given filed type
-            String strFieldType = aPageTemplateObject.field_type;
-            String fieldType = getCSSSelectorForGivenFieldType(strFieldType);
-
-            String strFieldName = aPageTemplateObject.field_name.toLowerCase().replaceAll(" ", "_");
-
-            // Create generic css selector based on section names, subsection names and field name
-            String cssSelectorSectionNames = "";
-            String strSectionName = "";
-
-            for (String sectionName : sectionNames) {
-                strSectionName = sectionName.toLowerCase().replaceAll(" ", "_");
-                cssSelectorSectionNames = cssSelectorSectionNames.concat(String.format("[data-name='%s'] ", strSectionName));
-            }
-
-            String createCssSelector = cssSelectorSectionNames + String.format("[data-name ='%s'] ", strFieldName)+ fieldType;
-
-            System.out.println(createCssSelector);
-
-            // Input random data based on type of input
-            WebElementFacade elementOnPage = newPage.element(By.cssSelector(createCssSelector));
-
-            // Check max length and if mandatory where available
-            if (!strFieldType.equalsIgnoreCase("image") && (!aPageTemplateObject.field_name.equalsIgnoreCase("link")||
-                    !elementOnPage.getAttribute("type").contains("link"))) {
-                if(elementOnPage.getAttribute("required") != null) {
-                    Assert.assertEquals(aPageTemplateObject.mandatory.toString(), elementOnPage.getAttribute("required"));
-                }
-            }
-
-            if(aPageTemplateObject.repeater != null)
-            {
-                if (!aPageTemplateObject.repeater.equals(""))
-                {
-                    // Click add button
-                    if(!oldSectionName.equals(sectionNames[0]))
-                    {
-                        String addButtonCSSSelector = String.format("[data-name='%s'] ", sectionNames[0].toLowerCase().replaceAll(" ", "_"));
-                        String copySectionName = addButtonCSSSelector;
-                        addButtonCSSSelector = addButtonCSSSelector.concat("[class='acf-button button button-primary'][data-event='add-row']");
-                        List<WebElementFacade> addButtons = newPage.findAll(By.cssSelector(addButtonCSSSelector));
-
-                        if(addButtons.size() > 0) {
-
-                            List <WebElementFacade> existingRowCount = newPage.findAll(By.cssSelector(copySectionName.concat("[class='acf-repeater -row'] [class='acf-row']")));
-
-                            for (int x = 0; x < Integer.parseInt(aPageTemplateObject.repeater) - existingRowCount.size(); x++) {
-
-                                if (addButtons.get(0).isCurrentlyVisible())
-                                    addButtons.get(0).click();
-                            }
-
-                            WebDriverWait wait = new WebDriverWait(allPages.getDriver(), 10);
-                            String checkAddItemDisabled = Serenity.sessionVariableCalled("Ignore disable add item");
-                            if(checkAddItemDisabled == null)
-                            {
-                                wait.until(ExpectedConditions.attributeContains(addButtons.get(0), "class", "disabled"));
-                            }
-                        }
-
-                        oldSectionName = sectionNames[0];
-
-                    }
-
-                    List<WebElementFacade> elementsOnPage = newPage.findAll(By.cssSelector(createCssSelector));
-
-                    for (int x = 0; x < Integer.parseInt(aPageTemplateObject.repeater); x++) {
-                        reusableSteps.createAndEnterRandomData(elementsOnPage.get(x), strFieldType);
-                    }
-                }
-                else
-                {
-                    reusableSteps.createAndEnterRandomData(elementOnPage, strFieldType);
-                }
-            }
-            else
-            {
-                reusableSteps.createAndEnterRandomData(elementOnPage, strFieldType);
-            }
-
-        }
-
-    }*/
-
     @Step
     public void openPage()
     {
@@ -387,12 +294,24 @@ public class PageSteps {
         boolean newSection = false;
 
         for (PageTemplateObject aPageTemplateObject : pageTemplateObject) {
-            String sectionNames = aPageTemplateObject.sections_sub_sections.split(",")[0];
+            Utility utility = new Utility();
+            String sectionNames = "";
+            if(utility.getSerenityPropertiesValues("webdriver.driver").equals("appium"))
+
+            {
+                sectionNames = aPageTemplateObject.sections_sub_sections.split(",")[0];
+            }
+            else
+            {
+                sectionNames = aPageTemplateObject.sections_sub_sections.replaceAll("," , "").replaceAll(" ","");
+            }
+
             if (!oldSectionName.equals(sectionNames)) {
                 oldSectionName = sectionNames;
                 newSection = true;
             } else {
                 newSection = false;
+
             }
 
             templateSectionSteps.selectAndFillTemplateSection(aPageTemplateObject, newSection);
